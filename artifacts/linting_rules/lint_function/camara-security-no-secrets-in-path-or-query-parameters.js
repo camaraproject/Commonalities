ee82212c6c3f0ac2f4fa9254d1d8f786990a27c7
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-12-11 13:03:02 +0530

    Draft/api linting implementation guideline (#1)
    * Create .spectral.yml
    
    * Create camara-language-avoid-telco.js
    
    * Create camara-reserved-words.js
    
    * Create camara-security-no-secrets-in-path-or-query-parameters.js
    
    * Create megalinter.yml
    
    * Create API-linting-Implementation-Guideline.md
    
    * update API linting document files with new links of files
    
    * Update API-linting-Implementation-Guideline.md
    
    * Create javalint.xml
    
    * Create .yamllint.yaml
    
    * reserved word 'enum' and 'default' are removed from reserved word array
    
    * Remove reserved words from arrays: openAPIKeywords and reservedWords"
    
    * Enable recommended option true/false for each rule.
    
    * Update log of telco rule
    
    * Update log of reserved word rule
    
    * Update  log of camara-security-no-secrets-in-path-or-query-parameters.js
    
    * update custom function rule name
    
    * update rule name as per camara standards

---
+++ artifacts/linting_rules/lint_function/camara-security-no-secrets-in-path-or-query-parameters.js
@@ -0,0 +1,23 @@
+const sensetiveData = ['MSISDN','IMSI'];
+
+export default async function (input) {
+
+  // Iterate over properties of the input object
+  for (const path in input) {
+
+    if (typeof path === 'string') {
+      for (const word of sensetiveData) {
+        const regex = new RegExp(`\\b${word}\\b`, 'g');  // Use a regular expression to match 'word' as a standalone word
+
+           if (regex.test(path)) {
+
+              const warningRuleName = 'camara-security-no-secrets-in-path-or-query-parameters';
+              const description = `Sensetive Data found in path: Consider avoiding the use of Sesentive data '${word}'`;
+              const location = `paths.${path}`;
+              console.log(`warning  ${warningRuleName}  ${description}  ${location}`);
+
+        }
+      }
+    }
+  }
+}
