c42c43b0fd25b714becee3f4f9f847263aa096f0
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-12-13 15:54:44 +0530

    Update Sensitive spelling from function

--- artifacts/linting_rules/lint_function/camara-security-no-secrets-in-path-or-query-parameters.js
+++ artifacts/linting_rules/lint_function/camara-security-no-secrets-in-path-or-query-parameters.js
@@ -6,13 +6,13 @@ export default async function (input) {
   for (const path in input) {
 
     if (typeof path === 'string') {
-      for (const word of sensetiveData) {
+      for (const word of sensitiveData ) {
         const regex = new RegExp(`\\b${word}\\b`, 'g');  // Use a regular expression to match 'word' as a standalone word
 
            if (regex.test(path)) {
 
               const warningRuleName = 'camara-security-no-secrets-in-path-or-query-parameters';
-              const description = `Sensetive Data found in path: Consider avoiding the use of Sesentive data '${word}'`;
+              const description = `sensitiveData  Data found in path: Consider avoiding the use of sensitiveData  data '${word}'`;
               const location = `paths.${path}`;
               console.log(`warning  ${warningRuleName}  ${description}  ${location}`);
 
